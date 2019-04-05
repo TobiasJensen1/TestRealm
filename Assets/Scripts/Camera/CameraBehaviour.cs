a9856b069deb438d918b28faa0b07b6495a2d817
tobbe <tobbejj@hotmail.com>
2019-04-05 14:22:35 +0200

    M

---
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    public GameObject player;
+
+    float z;
+    float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3(transform.position.x + player.transform.position.x, transform.position.y + player.transform.position.y, transform.position.z);
+
+        transform.position = new Vector3(
+   player.transform.position.x + -5,
+   transform.position.y,
+   player.transform.position.z + 0);
+        transform.LookAt(player.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x = ((player.transform.position.x + -5 - transform.position.x)) / 2;
+        z = ((player.transform.position.z + 0 - transform.position.z)) / 2;
+        transform.position += new Vector3((x * 10 * Time.deltaTime), 0, (z * 10 * Time.deltaTime));
+        
+        
+    }
+
+
+    
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float smooth;
+
+    Vector3 moveTo;
+    Quaternion targetRotation;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        targetRotation = transform.rotation;
+        GetComponent<Animation>().Play("Glory_01_Idle_01");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movement();
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, speed * Time.deltaTime);
+        transform.LookAt(moveTo, Vector3.up);
+        
+        
+    }
+
+
+    
+
+
+
+    void movement()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+            {
+                //changes animaton to run, changes player target position to hit.point, locks y position
+                GetComponent<Animation>().Play("Glory_01_Run_01");
+                moveTo = hit.point;
+                moveTo = new Vector3(hit.point.x, 0, hit.point.z);
+            }
+        }
+        
+        if(Vector3.Distance(transform.position, moveTo) < 0.001f)
+        {
+            GetComponent<Animation>().Play("Glory_01_Idle_01");
+        }
+        
+    }
+
+    
+
+
+
+
+}
