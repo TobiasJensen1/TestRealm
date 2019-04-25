792d62a69ec51723f0d84b50fef8bc3b87c39725
Your Name <you@example.com>
2019-04-26 01:35:38 +0200

    said drys

---
+++ Assets/Butterfly/Animation/AnimScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimScript : MonoBehaviour
+{
+
+    Tween tween;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        StartCoroutine("Anim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Anim()
+    {
+        while (true)
+        {
+            tween = transform.DOMoveX(transform.position.x+0.8f, 1.5F);
+            yield return tween.WaitForCompletion();
+            tween = transform.DOMoveX(transform.position.x-0.8f, 1.5F);
+            yield return tween.WaitForCompletion();
+        }
+    }
+}
